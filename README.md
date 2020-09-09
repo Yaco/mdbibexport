64a28ee78520f013448d16546fef3cc6f0350353
Franco Iacomella <franco@reevo.org>
2020-09-09 10:23:00 -0300

    change to non-interactive mode, taking files as cli arguments

--- README.md
+++ README.md
@@ -2,19 +2,34 @@
 
 mdbibexport.pl extracts the cited references of a Pandoc markdown document and writes a bibtex database for this document. The keys are extracted from the markdown file and written into an auxiliary file, which is used by BibTool to find the references in the bibtex (.bib) data base and to write them into a new file.
 
+This is a small fork that removes the prompts for manual input of files and takes the arguments passed to the command. See below.
+
 ## Requirements
 BibTool <http://gerd-neugebauer.de/software/TeX/BibTool/en/>, Pandoc <http://www.pandoc.org/> and Perl <https://www.perl.org/> have to be installed on the system. Version 2.65 of BibTool, version 1.16.0.2 of Pandoc and version 5.25 of Perl have been tested with this script.
 
 ## Use
-The Perl script is run from command line:  
-$perl mdbibexport.pl  
-The name of the markdown .md file and the bibliography .bib file will be asked. The cited references will be exported as mdbibexport.bib. Temporary and auxiliary files will be named as mdbibexport.*.
+The Perl script is run from command line, in a non interactive way, so you add the :  
+
+```
+$ perl mdbibexport.pl <path to markdown file> <path to bibtex file>
+```
+
+The cited references will be exported as <markdownd file>.bib.
+
+### Note for Calibre users
+
+To avoid the issue of change of cases in references, important if you are using Calibre to generate your .bib file, add these lines in your ~/.bibtoolrsc file:
+
+```
+preserve.key.case {on}
+select.case.sensitive {on}
+```
 
 ## License
 General Public License (GPL), version 3.0 <https://www.gnu.org/licenses/gpl-3.0.html>.
 
 ## Release version
-0.20.
+0.21
 
 ## Release date
 2016/12/11.
@@ -22,8 +37,10 @@ General Public License (GPL), version 3.0 <https://www.gnu.org/licenses/gpl-3.0.
 ## Author
 Dr. Robert Winkler.
 
+
 ## Contributor
 Albert Krewinkel.
+Franco Augusto
 
 ## Contact
 robert.winkler@bioprocess.org, robert.winkler@cinvestav.mx.
--- mdbibexport.pl
+++ mdbibexport.pl
@@ -1,52 +1,61 @@
 #!/usr/bin/perl
 #Simple script to extract references from a bibtex database which are cited in a markdown file
-#The installation of BibTool, Pandoc and Perl is required
-#Version: 0.2
+#The installation of BibTool, Pandoc and Perl is required. Non-interactive version.
+#Version: 0.21
 #Release: 2016/10/31
 #Copyright: Dr. Robert Winkler
-#Contributor: Albert Krewinkel
+#Contributor: Albert Krewinkel, Franco Augusto
 #Contact: robert.winkler@bioprocess.org, robert.winkler@cinvestav.mx
 #License: General Public License (GPL), 3.0
 
 use strict;
 use warnings;
 
-print "Extract from markdown (.md) file: ";
-my $namemd = <STDIN>;
-chomp($namemd);
-print "Extract from bibtex database (.bib) file: ";
-my $namebib = <STDIN>;
-chomp($namebib);
+if($ARGV[0] && $ARGV[1]){
 
-#mdbibexport files
-my $nameaux = "mdbibexport.aux";
-my $nameout = "mdbibexport.bib";
+    my $namemd = $ARGV[0];
+    chomp($namemd);
+    
+    my $namebib = $ARGV[1];
+    chomp($namebib);
+
+    #mdbibexport files
+    my $nameaux = "mdbibexport.aux";
+    my $nameout = "$ARGV[0].bib";
 
 
-open my $fm, '<', $namemd or die "Could not open '$namemd' $!\n";
+    open my $fm, '<', $namemd or die "Could not open '$namemd' $!\n";
 
-my $mdcitations = "";
+    my $mdcitations = "";
 
-my $pdcmd = 'pandoc -t json '. $namemd . ' > mdbibexport.tmp';
-system($pdcmd);
-my $pandoctmp = "mdbibexport.tmp";
-open my $fm2, '<', $pandoctmp or die "Could not open '$pandoctmp' $!\n";
+    my $pdcmd = 'pandoc -t json '. $namemd . ' > mdbibexport.tmp';
+    system($pdcmd);
+    my $pandoctmp = "mdbibexport.tmp";
+    open my $fm2, '<', $pandoctmp or die "Could not open '$pandoctmp' $!\n";
 
-while (my $line = <$fm2>) {
-   chomp $line;
-   while ($line =~ /"citationId":"([^"]*)"/g){
-   print "Extracted citation: $1\n";
-   $mdcitations .= "$1,";
+    while (my $line = <$fm2>) {
+    chomp $line;
+    while ($line =~ /"citationId":"([^"]*)"/g){
+    print "Extracted citation: $1\n";
+    $mdcitations .= "$1,";
+        }
     }
+
+    open(my $fa, '>', $nameaux) or die;
+    print $fa "\\bibstyle{alpha}\n";
+    print $fa "\\bibdata{$namebib}\n";
+    print $fa "\\citation{$mdcitations}\n";
+    close $fa;
+    print "$nameaux created.\n";
+
+    my $btcmd = 'bibtool -v -x '. $nameaux . ' -o ' . $nameout;
+    system($btcmd);
+    print "$nameout created.\n";
+
+    unlink "mdbibexport.tmp";
+    unlink "mdbibexport.aux";
+}else{
+    print "Missing arguments: mdbibexport <path to .md> <path to .bib>\n";
+
 }
 
-open(my $fa, '>', $nameaux) or die;
-print $fa "\\bibstyle{alpha}\n";
-print $fa "\\bibdata{$namebib}\n";
-print $fa "\\citation{$mdcitations}\n";
-close $fa;
-print "$nameaux created.\n";
-
-my $btcmd = 'bibtool -v -x '. $nameaux . ' -o ' . $nameout;
-system($btcmd);
-print "$nameout created.\n";
